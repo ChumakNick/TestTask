9505cf8d23ee73fd3bfd3fb1732cafea57a7cf97
Nikita <chumaktruesight@yandex.ru>
2020-12-18 17:07:27 +0500

    Initialize

---
+++ src/main/java/com/example/testtask/archibarius/ArchibariusApplication.java
@@ -0,0 +1,13 @@
+package com.example.testtask.archibarius;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchibariusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchibariusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/testtask/archibarius/ArchibariusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testtask.archibarius;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchibariusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
